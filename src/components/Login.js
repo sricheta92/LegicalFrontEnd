9e1c978f56965d52e1b97392a08ace40ed0a3233
zenobia panvelwalla <zenobiapanvelwalla@gmail.com>
2019-04-06 15:19:36 -0700

    Login page

---
+++ src/components/Login.js
@@ -0,0 +1,139 @@
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import login from "../custom_css/login.css";
+import config from '../config.js';
+import axios from 'axios';
+
+
+class Login extends Component {
+
+    state={
+        userdata: {
+            email: '',
+            password: '',
+        },
+        emailValid: true,
+        passwordValid: true,
+        msgResult:true
+    }
+
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        console.log("Call to page ");
+        if(this.validateEmail() === true)
+        {
+            if(this.validatePassword() === true)
+            {
+                var data = {
+                    "email":this.refs.email.value,
+                    "password":this.refs.password.value
+                    }
+                let self = this;
+                console.log("User Data:" + JSON.stringify(data))
+            }
+            else
+            {
+                this.setState({passwordValid: false})
+            }
+        }
+        else
+        {
+            //this.state.usernameValid = false;
+            this.setState({emailorusernameValid: false})
+        }
+    }
+
+    validateEmail() {
+        var email = this.refs.email.value;
+        if (email !== '')
+        {
+            return (true)
+        }
+        return (false)
+    }
+    validatePassword(){
+        var password = this.refs.password.value;
+        if (password !== '')
+        {
+            return (true)
+        }
+        return (false)
+    }
+
+    render() {
+        return (
+            <div className="backgroundLogin">
+                <div className="container">
+                <div className="d-flex justify-content-center h-100">
+                    <div className="cardL card">
+                        <div className="cardL-header card-header">
+                            <br/>
+                            <h3>Sign In</h3>
+                            {/* <div className="d-flex justify-content-end social_icon">
+                                <span><i className="fab fa-facebook-square"></i></span>
+                                <span><i className="fab fa-google-plus-square"></i></span>
+                                <span><i className="fab fa-twitter-square"></i></span>
+                            </div> */}
+                        </div>
+                        <div >
+                            {this.props.loginMsg && (
+                                <div className="alert alert-warning text-danger small" role="alert">
+                                    {this.props.loginMsg}
+                                </div>
+                            )}
+                        </div>
+                        <div className="card-body">
+                        { this.state.msgResult ? null : <div className="text-left text-small small alert alert-warning">Username or Password not found.</div>}
+                        { this.state.emailorusernameValid ? null : <div className="text-left text-small text-white">Username is required.</div>}
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fas fa-user"></i></span>
+                                    </div>
+                                    <input type="text" name = "email" className="form-control" placeholder="email"
+                                           ref="email"
+                                           onFocus={(event) => {
+                                           this.setState({emailorusernameValid: true, msgResult : true});
+                                    }} />
+
+                                </div>
+                                { this.state.passwordValid ? null : <div className="text-left text-small text-white">Password is required.</div>}
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fas fa-key"></i></span>
+                                    </div>
+                                    <input name = "password" type="password" className="form-control" placeholder="password"
+                                           ref = "password"
+                                           onFocus={(event) => {
+                                               this.setState({passwordValid: true , msgResult: true});
+                                           }}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <button name="Login" value="Login" className="btn float-right login_btn"
+                                           onClick={(event) => {
+                                               this.handleSubmit(event)
+                                           }}>Sign In</button>
+                                </div>
+                        </div>
+                        <div className="card-footer">
+                            <div className="d-flex justify-content-center links">
+                                Don't have an account?<a href="signup">Register</a>
+                            </div>
+                            <div className="d-flex justify-content-center">
+                                <a href="signup">Forgot your password?</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        );
+    }
+}
+export default withRouter(Login);
+
